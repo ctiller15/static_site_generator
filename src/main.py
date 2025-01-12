4b33f8ea778f7a98af75c9bbd76cc7fc353d3c91
ctiller15 <christiller2015@gmail.com>
2025-01-11 20:15:19 -0500

    add public directory with file copying from static

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from textnode import TextNode, TextType
 
 
@@ -6,7 +7,19 @@ def copy_directory(dir_name, target_dir_name):
     if os.path.exists(dir_name):
         dir_items = os.listdir(dir_name)
         for item in dir_items:
-            print(os.path.join(item))
+            source_path = os.path.join(*[dir_name, item])
+            target_path = os.path.join(*[target_dir_name, item])
+            if os.path.exists(source_path):
+                if os.path.isfile(source_path):
+                    # add file from source to target dir.
+                    shutil.copy(source_path, target_path)
+                else:
+                    if os.path.exists(target_path):
+                        shutil.rmtree(target_path)
+
+                    os.mkdir(target_path)
+                    # recurse with the new dir name.
+                    copy_directory(source_path, target_path)
         
 
 def main():
